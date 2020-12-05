77a1d9451a8b3b9629d49d1b0368b06f6de8afb4
Anthony <70252860+anthonyvanderwal@users.noreply.github.com>
2020-12-05 20:56:41 +0800

    load table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,7 +1,19 @@
 // from data.js
 var tableData = data;
+var columns = ['datetime', 'city', 'state', 'country', 'shape', 'durationMinutes', 'comments'];
 
-// YOUR CODE HERE!
-d3.select('thead').attr('class', 'thead-dark');
+// table body
+var tbody = d3.select('tbody');
 
+// append rows 
+function tabulate() { 
+    tableData.forEach( sighting => {
+        var row = tbody.append('tr')
+        columns.forEach( column => { 
+            row.append('td').text(sighting[column])
+        })
+    })
+};
 
+// load table data 
+tabulate();
